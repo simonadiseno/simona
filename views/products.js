9441db5c5931e8e618e95133548a820b333b212b
Hernan Llull <hernanllull@gmail.com>
2020-06-29 16:25:00 -0300

    -

---
+++ views/products.js
@@ -0,0 +1,243 @@
+var products = [
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565801/Tablas/Cuenco_Suria_aosvx5.jpg",
+    name: "Cuenco Suria",
+    price: 190,
+    category: "Tablas y cuencos",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565801/Tablas/Tabla_esmeralda_300_aqtrtr.jpg",
+    name: "Tabla Esmeralda",
+    price: 190,
+    category: "Tablas y cuencos",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565801/Tablas/Cuencos_de_madera_jmuka1.jpg",
+    name: "Cuencos de madera",
+    price: 190,
+    category: "Tablas y cuencos",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565801/Tablas/Cuenco_Grecia_hruafm.jpg",
+    name: "Cuenco Grecia",
+    price: 190,
+    category: "Tablas y cuencos",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565801/Tablas/Cuenco_Dana_190_lyfhvx.jpg",
+    name: "Cuenco Dana",
+    price: 190,
+    category: "Tablas y cuencos",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565883/Mates/Modelo_Sofia_jlnc5f.jpg",
+    name: "Modelo Sofia",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565882/Mates/Modelo_mandala_10_hw6x81.jpg",
+    name: "Modelo Mandala",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565881/Mates/Modelo_Mandala_zt1dz0.jpg",
+    name: "Modelo Mandala",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565881/Mates/Modelo_mandala_11_pvuk9i.jpg",
+    name: "Modelo Mandala",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565880/Mates/Modelo_Pia_dxanu7.jpg",
+    name: "Modelo Pia",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565878/Mates/Modelo_norte_frplne.jpg",
+    name: "Modelo Norte",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565875/Mates/Mate_Justina_2_dmwkza.jpg",
+    name: "Modelo Justina",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565875/Mates/Modelo_Frida_zpev8k.jpg",
+    name: "Modelo Frida",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565874/Mates/Modelo_Julia_rom979.jpg",
+    name: "Modelo Julia",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565874/Mates/Modelo_mandala_4_bx2pru.jpg",
+    name: "Modelo Mandala",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565874/Mates/Modelo_mandala_2_huoe3y.jpg",
+    name: "Modelo Mandala",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565874/Mates/Modelo_Luisina_ljk4du.jpg",
+    name: "Modelo Luisina",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565869/Mates/Modelo_Indi_fb7rb5.jpg",
+    name: "Modelo Indi",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565869/Mates/Modelo_esmeralda_kc7b9o.jpg",
+    name: "Modelo Esmeralda",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565869/Mates/Modelo_Frida_2_uh18rt.jpg",
+    name: "Modelo Frida",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565868/Mates/Modelo_cactus_rcvesz.jpg",
+    name: "Modelo Cactus",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565867/Mates/Modelo_Ana_mybtwy.jpg",
+    name: "Modelo Ana",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565866/Mates/Mates_flores_s4ijou.jpg",
+    name: "Modelo Flores",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565866/Mates/Mate_Justina_wbz933.jpg",
+    name: "Modelo Justina",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565865/Mates/Mate_bocon_octavia_350_bb0dab.jpg",
+    name: "Modelo Bocon Octavia",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565865/Mates/Mate_Emma_hnxus2.jpg",
+    name: "Modelo Emma",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565863/Mates/Mate_Clara_gcycyu.jpg",
+    name: "Modelo Clara",
+    price: 390,
+    category: "Mates",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591565863/Mates/Mate_Chiara_650_w46mmm.jpg",
+    name: "Modelo Chiara",
+    price: "650",
+    category: "Mates",
+  },
+
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566789/Personalizados/Fotografia_Fran_ituknq.jpg",
+    name: "Fotogrfía Fran",
+    price: 390,
+    category: "Personalizados",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566788/Personalizados/Roma_Depiladora_hpncyw.jpg",
+    name: "Roma Depiladora",
+    price: 390,
+    category: "Personalizados",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566788/Personalizados/Frida_Accesorios_r2g0cv.jpg",
+    name: "Frida Accesorios",
+    price: 390,
+    category: "Personalizados",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566787/Personalizados/Dulce_Merce_350_yt5789.jpg",
+    name: "Dulce Merce",
+    price: 390,
+    category: "Personalizados",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566786/Personalizados/Logo_Pink_Floyd_pxjdbb.jpg",
+    name: "Logo Pink Floyd",
+    price: 390,
+    category: "Personalizados",
+  },
+  {
+    url:
+      "https://res.cloudinary.com/dxs2nwz4y/image/upload/v1591566785/Personalizados/Homero_Simpson_ikzlzq.jpg",
+    name: "Homero Simpson",
+    price: 390,
+    category: "Personalizados",
+  },
+];
+
+module.exports = products;
